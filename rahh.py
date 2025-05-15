ab1d74bb1f75e8400b2eada0b5101d655339bad5
lucytrollphone <luc29v11@gmail.com>
2025-05-15 12:21:53 -0400

    Add files via upload

---
+++ rahh.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+@app.route('/ugc/group/<int:group_id>')
+def get_group_items(group_id):
+    url = f"https://catalog.roblox.com/v1/search/items?CreatorType=Group&CreatorTargetId={group_id}&Limit=50&category=11&Subcategory=21"
+    headers = { "User-Agent": "UGCProxy/1.0" }
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(port=5000)
